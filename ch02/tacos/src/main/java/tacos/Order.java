60f0d0facc4a3706e67fd7ff7fd305846b996e32
Mateusz Kulec <101333426+MateuszRL96@users.noreply.github.com>
2023-10-12 20:27:01 +0200

    gdfgxfdhg

--- ch02/tacos/src/main/java/tacos/Order.java
+++ ch02/tacos/src/main/java/tacos/Order.java
@@ -18,38 +18,38 @@ public class Order {
   private String name;
   //end::allButValidation[]
 
-  @NotBlank(message="Adres is required")
+  @NotBlank(message="Adres jest wymagane")
   //tag::allButValidation[]
   private String street;
   //end::allButValidation[]
 
-  @NotBlank(message="Miasto is required")
+  @NotBlank(message="Miasto jest wymagane")
   //tag::allButValidation[]
   private String city;
   //end::allButValidation[]
 
-  @NotBlank(message="Kraj is required")
+  @NotBlank(message="Kraj jest wymagane")
   //tag::allButValidation[]
   private String state;
   //end::allButValidation[]
 
-  @NotBlank(message="Numer Domu is required")
+  @NotBlank(message="Numer Domu jest wymagane")
   //tag::allButValidation[]
   private String zip;
   //end::allButValidation[]
 
-  @CreditCardNumber(message="Not a valid credit card number")
+  @CreditCardNumber(message="To nie jest prawidłowy format karty kredytowej")
   //tag::allButValidation[]
   private String ccNumber;
   //end::allButValidation[]
 
   @Pattern(regexp="^(0[1-9]|1[0-2])([\\/])([1-9][0-9])$",
-           message="Must be formatted MM/YY")
+           message="Musi być w formacie  MM/YY")
   //tag::allButValidation[]
   private String ccExpiration;
   //end::allButValidation[]
 
-  @Digits(integer=3, fraction=0, message="Invalid CVV")
+  @Digits(integer=3, fraction=0, message="Nieprawidłowy kod CVV")
   //tag::allButValidation[]
   private String ccCVV;
 
