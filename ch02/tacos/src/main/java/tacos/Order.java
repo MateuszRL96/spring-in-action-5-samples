d5af38ce1e175548ccf87a6be30444c51867a46b
Mateusz Kulec <101333426+MateuszRL96@users.noreply.github.com>
2023-10-18 22:22:10 +0200

    86

--- ch02/tacos/src/main/java/tacos/Order.java
+++ ch02/tacos/src/main/java/tacos/Order.java
@@ -1,6 +1,8 @@
 //tag::all[]
 //tag::allButValidation[]
 package tacos;
+import java.sql.Date;
+
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
@@ -11,6 +13,9 @@ import lombok.Data;
 
 @Data
 public class Order {
+
+  private Long id;
+  private Date createdAt;
 
   //end::allButValidation[]
   @NotBlank(message="Imie i Nazwisko jest wymagane")
